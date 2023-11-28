f463b4d8398f025c649e3387a45e32a80dd52de2
Weiwen58 <90655368+Weiwen58@users.noreply.github.com>
2023-11-28 11:44:05 -0600

    Add files via upload

---
+++ import.py
@@ -0,0 +1,223 @@
+import mysql.connector
+import pandas as pd
+import ast
+from sqlalchemy import create_engine
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="sqluser",
+  password="password",
+  database="movies_metadata"
+)
+mycursor = mydb.cursor()
+engine = create_engine('mysql+mysqlconnector://sqluser:password@localhost/movies_metadata')
+
+# main dataset
+df = pd.read_csv("./data/new_movies_metadata.csv")
+dic_columns = ["genres", "production_companies", "production_countries", "spoken_languages",
+               "keywords", "cast", "crew"]
+for c in dic_columns:
+    df[c] = df[c].apply(lambda x: ast.literal_eval(x) if pd.notnull(x) and x != '[]' else [])
+
+
+# movie table
+# movie_columns = ["id", "imdb_id", "title", "language", "isAdult", "releaseDate", "runtimeMinutes"]
+# movie_table = df[movie_columns]
+
+# movie_table.to_sql(name="movie", con=engine, if_exists="append", index=False)
+
+
+# ratings table
+# ratings_columns = ["id", "averageRating", "numVotes"]
+# ratings_table = df[ratings_columns].rename(columns={"id": "movieId"})
+
+# ratings_table.to_sql(name="ratings", con=engine, if_exists="append", index=False)
+
+
+# genres table
+# unique_genres = set()
+# for genres_list in df["genres"]:
+#   for genre in genres_list:
+#       unique_genres.add((genre.get("id"), genre.get("name")))
+
+# for id, name in unique_genres:
+#   query = "INSERT IGNORE INTO genres (id, name) VALUES (%s, %s);"
+#   mycursor.execute(query, (id, name))
+
+
+# movie_genre table
+# for _, row in df.iterrows():
+#   movie_id = row["id"]
+#   for genre in row["genres"]:
+#       genre_id = genre.get("id")
+#       query = "INSERT INTO movie_genres (movieId, genreId) VALUES (%s, %s);"
+#       mycursor.execute(query, (movie_id, genre_id))
+
+# mydb.commit()
+
+
+# spokenLanguage table
+# unique_languages = set()
+# for l_list in df["spoken_languages"]:
+#   for l in l_list:
+#       unique_languages.add((l.get("iso_639_1"), l.get("name")))
+
+# for language_code, name in unique_languages:
+#   query = "INSERT IGNORE INTO spokenLanguage (languageCode, name) VALUES (%s, %s);"
+#   mycursor.execute(query, (language_code, name))
+
+
+# productionCompanies table
+# unique_companies = set()
+# for company_list in df["production_companies"]:
+#   for company in company_list:
+#       unique_companies.add((company.get("id"), company.get("name")))
+
+# for id, name in unique_companies:
+#   query = "INSERT IGNORE INTO productionCompanies (id, name) VALUES (%s, %s);"
+#   mycursor.execute(query, (id, name))
+
+# mydb.commit()
+
+
+# productionCountries table
+# unique_countries = set()
+# for countries_list in df["production_countries"]:
+#   for country in countries_list:
+#       unique_countries.add((country.get("iso_3166_1"), country.get("name")))
+
+# for country_code, name in unique_countries:
+#   query = "INSERT IGNORE INTO productionCountries (countryCode, name) VALUES (%s, %s);"
+#   mycursor.execute(query, (country_code, name))
+
+# mydb.commit()
+
+
+# commercial table
+# commercial_columns = ["popularity", "budget", "revenue", "status", "id"]
+# commercial_table = df[commercial_columns].rename(columns={"id": "movieId"})
+# commercial_table["popularity"] = commercial_table["popularity"].round(5)
+
+# commercial_table.to_sql(name="commercial", con=engine, if_exists="append", index=False)
+
+
+# commercial_productionCompanies and commercial_productionCountries table
+# fetch_query = "SELECT id FROM movies_metadata.commercial WHERE movieId = %s;"
+
+# for _, row in df.iterrows():
+#   movie_id = row["id"]
+#   mycursor.execute(fetch_query, (movie_id,))
+#   commercial_id = mycursor.fetchone()
+  
+#   query = "INSERT IGNORE INTO commercial_productionCompanies (commercialId, productionCompanyId) VALUES (%s, %s);"
+#   for company in row["production_companies"]:
+#     company_id = company.get("id")
+#     mycursor.execute(query, (commercial_id[0], company_id))
+
+# mydb.commit()
+  
+#   query = "INSERT IGNORE INTO commercial_productionCountries (commercialId, productionCountryCode) VALUES (%s, %s);"
+#   for country in row["production_countries"]:
+#     country_code = country.get("iso_3166_1")
+#     mycursor.execute(query, (commercial_id[0], country_code))
+
+# mydb.commit()
+
+
+# details table
+# df = pd.read_csv("./new_basics.csv")
+# details_columns = ["original_title", "overview", "tagline", "startYear", "id"]
+# details_table = df[details_columns].rename(columns={"id": "movieId", "original_title": "originalTitle"})
+
+# details_table.to_sql(name="details", con=engine, if_exists="append", index=False)
+
+
+# details_spokenLanguage table
+# fetch_query = "SELECT detailId FROM movies_metadata.details WHERE movieId = %s;"
+
+# for _, row in df.iterrows():
+#   movie_id = row["id"]
+#   mycursor.execute(fetch_query, (movie_id,))
+#   detail_id = mycursor.fetchone()
+  
+#   query = "INSERT IGNORE INTO details_spokenLanguage (detailId, languageCode) VALUES (%s, %s);"
+#   for language in row["spoken_languages"]:
+#     language_code = language.get("iso_639_1")
+#     mycursor.execute(query, (detail_id[0], language_code))
+
+# mydb.commit()
+
+
+# keywords table
+# keywords_df = pd.read_csv("./data/keywords.csv")
+# dic_columns = ["keywords"]
+# for c in dic_columns:
+#     keywords_df[c] = keywords_df[c].apply(lambda x: ast.literal_eval(x) if x and x != '[]' else [])
+# unique_keywords = set()
+# for keywords_list in keywords_df["keywords"]:
+#   for keyword in keywords_list:
+#       unique_keywords.add((keyword.get("id"), keyword.get("name")))
+
+# for id, name in unique_keywords:
+#   query = "INSERT IGNORE INTO keywords (id, name) VALUES (%s, %s);"
+#   mycursor.execute(query, (id, name))
+
+# mydb.commit()
+
+# movie_keywords table
+# for _, row in df.iterrows():
+#   movie_id = row["id"]
+#   for keyword in row["keywords"]:
+#     keyword_id = keyword.get("id")
+#     query = "INSERT IGNORE INTO movie_keywords (movieId, keywordId) VALUES (%s, %s);"
+#     mycursor.execute(query, (movie_id, keyword_id))
+
+# mydb.commit()
+
+
+# actor and has_cast table
+# unique_casts = set()
+# unique_actors = set()
+# for _, row in df.iterrows():
+#   movie_id = row["id"]
+#   for cast in row["cast"]:
+#     unique_casts.add((cast.get("cast_id"), cast.get("character"), cast.get("credit_id"), cast.get("order"), cast.get("id"), movie_id))
+#     unique_actors.add((cast.get("id"), cast.get("name"), cast.get("gender")))
+
+# for cast_id, name, gender in unique_actors:
+#   query = "INSERT IGNORE INTO actors (id, name, gender) VALUES (%s, %s, %s);"
+#   mycursor.execute(query, (cast_id, name, gender))
+
+# for cast_id, character, credit_id, order, actorId, movieId in unique_casts:
+#   query = "INSERT IGNORE INTO has_cast (castId, `character`, creditId, `order`, actorId, movieId) VALUES (%s, %s, %s, %s, %s, %s);"
+#   mycursor.execute(query, (cast_id, character, credit_id, order, actorId, movieId))
+
+# mydb.commit()
+
+
+# crew table
+# unique_crews = set()
+# for crews_list in df["crew"]:
+#   for crew in crews_list:
+#     unique_crews.add((crew.get("id"), crew.get("job"), crew.get("department"), crew.get("credit_id"), crew.get("gender"), crew.get("name")))
+
+# for id, job, department, credit_id, gender, name in unique_crews:
+#   query = "INSERT IGNORE INTO crew (id, job, department, creditId, gender, name) VALUES (%s, %s, %s, %s, %s, %s);"
+#   mycursor.execute(query, (id, job, department, credit_id, gender, name))
+
+# mydb.commit()
+
+
+# has_crew table
+# for _, row in df.iterrows():
+#   movie_id = row["id"]
+#   for crew in row["crew"]:
+#     crew_id = crew.get("id")
+#     query = "INSERT IGNORE INTO has_crew (crewId, movieId) VALUES (%s, %s);"
+#     mycursor.execute(query, (crew_id, movie_id))
+
+# mydb.commit()
+
+
+mycursor.close()
+mydb.close()
---
+++ movies_database.sql
@@ -0,0 +1,140 @@
+CREATE DATABASE movies_metadata;
+USE movies_metadata;
+
+CREATE TABLE movie (
+    id INT PRIMARY KEY,
+    imdb_id VARCHAR(20),
+    title VARCHAR(255),
+    language VARCHAR(20),
+    isAdult BOOLEAN,
+    releaseDate DATE,
+    runtimeMinutes INT
+);
+
+CREATE TABLE genres (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE movie_genres (
+    movieId INT,
+    genreId INT,
+    FOREIGN KEY (movieId) REFERENCES movie(id),
+    FOREIGN KEY (genreId) REFERENCES genres(id)
+);
+
+CREATE TABLE keywords (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE movie_keywords (
+    movieId INT,
+    keywordId INT,
+    FOREIGN KEY (movieId) REFERENCES movie(id),
+    FOREIGN KEY (keywordId) REFERENCES keywords(id)
+);
+
+CREATE TABLE ratings (
+    movieId INT PRIMARY KEY,
+    averageRating DECIMAL(3,1),
+    numVotes INT,
+    FOREIGN KEY (movieId) REFERENCES movie(id)
+);
+
+CREATE TABLE details (
+    detailId INT PRIMARY KEY AUTO_INCREMENT,
+    originalTitle VARCHAR(255),
+    overview VARCHAR(1024),
+    tagline VARCHAR(1024),
+    startYear INT,
+    movieId INT UNIQUE,
+    FOREIGN KEY (movieId) REFERENCES movie(id)
+);
+
+CREATE TABLE spokenLanguage (
+    languageCode VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE details_spokenLanguage (
+    detailId INT,
+    languageCode VARCHAR(20),
+    FOREIGN KEY (detailId) REFERENCES details(detailId),
+    FOREIGN KEY (languageCode) REFERENCES spokenLanguage(languageCode)
+);
+
+CREATE TABLE commercial (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    popularity DECIMAL(5,10),
+    budget INT,
+    revenue INT,
+    status VARCHAR(20),
+    movieId INT UNIQUE,
+    FOREIGN KEY (movieId) REFERENCES movie(id)
+);
+
+CREATE TABLE productionCompanies (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE productionCountries (
+    countryCode VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE commercial_productionCompanies (
+    commercialId INT,
+    productionCompanyId INT,
+    FOREIGN KEY (commercialId) REFERENCES commercial(id),
+    FOREIGN KEY (productionCompanyId) REFERENCES productionCompanies(id)
+);
+
+CREATE TABLE commercial_productionCountries (
+    commercialId INT,
+    productionCountryCode VARCHAR(20),
+    FOREIGN KEY (commercialId) REFERENCES commercial(id),
+    FOREIGN KEY (productionCountryCode) REFERENCES productionCountries(countryCode)
+);
+
+CREATE TABLE actors (
+    id INT PRIMARY KEY,
+    name VARCHAR(64),
+    gender VARCHAR(20)
+);
+
+CREATE TABLE has_cast (
+    castId INT,
+    `character` VARCHAR(64),
+    creditId VARCHAR(32),
+    `order` INT,
+    actorId INT,
+    movieId INT,
+    PRIMARY KEY (actorId, movieId),
+    FOREIGN KEY (actorId) REFERENCES actors(id),
+    FOREIGN KEY (movieId) REFERENCES movie(id)
+);
+
+CREATE TABLE crew (
+    id INT PRIMARY KEY,
+    job VARCHAR(32),
+    department VARCHAR(32),
+    creditId VARCHAR(32),
+    gender VARCHAR(20),
+    name VARCHAR(64)
+);
+
+CREATE TABLE has_crew (
+    crewId INT,
+    movieId INT,
+    PRIMARY KEY (crewId, movieId),
+    FOREIGN KEY (crewId) REFERENCES crew(id),
+    FOREIGN KEY (movieId) REFERENCES movie(id)
+);
+
+SHOW TABLES;
+
+-- DELETE FROM movies_metadata.details_spokenLanguage;
+-- DELETE FROM movies_metadata.productioncountries;
+-- USE movies_metadata;
