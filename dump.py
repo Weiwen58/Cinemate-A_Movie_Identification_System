5f053258a3e411bde9c8a203c9c7dd82c798c3f1
Y-Elsayed <108357222+Y-Elsayed@users.noreply.github.com>
2023-12-08 21:11:41 +0200

    Normalized hte tfidf matrix

--- dump.py
+++ dump.py
@@ -20,7 +20,7 @@ movie_ids = [data[0] for data in tokenized_movies]
 tokenized_overviews = [data[1] for data in tokenized_movies]
 
 # Initialize the TF-IDF vectorizer
-tfidf_vectorizer = TfidfVectorizer()
+tfidf_vectorizer = TfidfVectorizer(norm='l2', smooth_idf=False)
 
 # Fit and transform the movie overviews to TF-IDF vectors
 tfidf_matrix = tfidf_vectorizer.fit_transform(tokenized_overviews)
@@ -31,7 +31,7 @@ with open('movie_ids.pkl', 'wb') as movie_ids_file:
     pickle.dump(movie_ids, movie_ids_file)
     
 with open('tfidf_matrix.pkl', 'wb') as matrix_file:
-    pickle.dump(tfidf_matrix, matrix_file)
+    pickle.dump(tfidf_matrix_normalized, matrix_file)
 
 with open('tfidf_vectorizer.pkl', 'wb') as vectorizer_file:
     pickle.dump(tfidf_vectorizer, vectorizer_file)
