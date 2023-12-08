9ee6a632a2f21c8bb2ea4e165afc5ed311d8ed90
Y-Elsayed <108357222+Y-Elsayed@users.noreply.github.com>
2023-12-08 20:35:37 +0200

    Refined the string cleaning
    - string_cleaning file
     -Created functions to remove punctuation, stop words and stem verbs
     -A function that returns a string and another that returns a list
     -Cleaned the overview tokens before inserting them into the weak entity
     -Used the new funciton in search_engine.py
     -stop_words_downloader should be run 1 time to download the stop word dataset

---
+++ stop_words_downloader.py
@@ -0,0 +1,7 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# Download the stopwords dataset if not already present
+nltk.download('stopwords')
+nltk.download('punkt')
